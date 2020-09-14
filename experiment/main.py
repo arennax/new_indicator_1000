b68e0bfc95349a8ca207ad371396c3257702da29
arennax <xiatianpei@gmail.com>
2020-09-14 11:42:08 -0400

    update goal options

--- experiment/main.py
+++ experiment/main.py
@@ -89,20 +89,6 @@ def prediction(Repo, Directory, metrics, repeats, goal, month, tocsv):
             if tocsv:
                 with open("../result_experiment/goal{}.csv".format(goal), "a+") as output:
                     output.write(str(np.median(list_output)) + "\n")
-        #
-        # if way == 6:
-        #     list_temp = []
-        #     for way in range(repeats):
-        #         list_temp.append(CART_FLASH(data, metrics, month))
-        #
-        #     flat_list = np.array(list_temp).flatten()
-        #     list_output = sorted(flat_list.tolist())
-        #
-        #     print("median for ROME:", np.median(list_output))
-        #     if tocsv:
-        #         with open("./zgoal{}.csv".format(goal), "a+") as output:
-        #             output.write(str(np.median(list_output)) + "\n")
-
 
 
 if __name__ == '__main__':
@@ -121,13 +107,25 @@ if __name__ == '__main__':
     Goal = 0
     month = 1
 
+    # goal == 0: 'number_of_commits'
+    # goal == 1: 'number_of_contributors'
+    # goal == 2: 'number_of_new_contributors'
+    # goal == 3: 'number_of_contributor-domains'
+    # goal == 4: 'number_of_new_contributor-domains'
+    # goal == 5: 'number_of_open_PRs'
+    # goal == 6: 'number_of_closed_PRs'
+    # goal == 7: 'number_of_open_issues'
+    # goal == 8: 'number_of_closed_issues'
+    # goal == 9: 'number_of_stargazers'
+
     time_begin = time.time()
 
     number = 0
     for i in range(10):
         for repo in sorted(repo_pool):
-            print(number, repo)
-            prediction(repo, path, Metrics, Repeats, i, month, tocsv=True)
+            print("-----------------------------------------")
+            print(number, repo, "Goal:", i)
+            prediction(repo, path, Metrics, Repeats, goal=i, month=1, tocsv=False)
             number += 1
 
     run_time = str(time.time() - time_begin)
